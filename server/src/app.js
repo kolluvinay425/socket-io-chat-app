f39e532d5fe75e34dbf868f48669d58d88156f18
kolluvinay425 <kolluvinay425@gmail.com>
2021-11-16 15:02:06 +0100

    update

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import si from "systeminformation";
 import userRouter from "./users/index.js";
 import {
   unauthorizedHandler,
@@ -8,29 +7,7 @@ import {
   catchAllHandler,
 } from "./errorHandlers.js";
 
-// promises style - new since version 3
-// si.system()
-//   .then((data) => console.log("system details", data))
-//   .catch((error) => console.error(error));
-import QRCode from "qrcode";
-// export const generateQR = async (text) => {
-//   try {
-//     const qr = await QRCode.toDataURL(text);
-//     console.log(qr);
-//   } catch (err) {
-//     console.error(err);
-//   }
-// };
-// generateQR("vinay");
-// let segs = [
-//   { data: "ABCDEFG", mode: "alphanumeric" },
-//   { data: "0123456", mode: "numeric" },
-// ];
-// QRCode.toDataURL(segs, { version: 2 }, function (err, url) {
-//   console.log("dacbdjkb", url);
-// });
 export const app = express();
-// const port = 3030;
 app.use(cors());
 app.use(express.json());
 
