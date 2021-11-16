c544e5f4b8ab40df109faa53e8d0aefb1478ef5c
kolluvinay425 <kolluvinay425@gmail.com>
2021-11-16 12:11:54 +0100

    update

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,33 @@
 import express from "express";
 import cors from "cors";
+import si from "systeminformation";
 
+// promises style - new since version 3
+// si.system()
+//   .then((data) => console.log("system details", data))
+//   .catch((error) => console.error(error));
+import QRCode from "qrcode";
+// export const generateQR = async (text) => {
+//   try {
+//     const qr = await QRCode.toDataURL(text);
+//     console.log(qr);
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
+// generateQR("vinay");
+// let segs = [
+//   { data: "ABCDEFG", mode: "alphanumeric" },
+//   { data: "0123456", mode: "numeric" },
+// ];
+// QRCode.toDataURL(segs, { version: 2 }, function (err, url) {
+//   console.log("dacbdjkb", url);
+// });
 export const app = express();
-const port = 3030;
+// const port = 3030;
 app.use(cors());
 app.use(express.json());
 
-app.get("/chat/:room", async (req, res) => {});
+// app.get("/chat/:room", async (req, res) => {});
 
 export default app;
--- server/src/index.js
+++ server/src/index.js
@@ -15,9 +15,9 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
-  socket.on("join_room", (data) => {
-    socket.join(data);
-    console.log(`userId with ${socket.id} joined room:${data}`);
+  socket.on("join_room", (roomId, username) => {
+    socket.join(roomId);
+    console.log(`userName with ${username} joined room:${roomId}`);
   });
   socket.on("send_message", (data) => {
     socket.to(data.room).emit("receive_message", data);
